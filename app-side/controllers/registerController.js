3637e45c0422bea17724cb8257035c350ca9ee6d
lucasjcordoba <lucasjcordoba@gmail.com>
2020-08-18 08:46:53 -0300

    iniciando express-validator

--- app-side/controllers/registerController.js
+++ app-side/controllers/registerController.js
@@ -1,16 +1,20 @@
 let db = require('../database/models')
-
+let {check, validationResult, body} = require('express-validator');
 
 let registerController = {
     register: function(req, res){
         res.render('register', {title:'register'})
     },
-    newUSer:function(req, res){
-        db.User.create({
+    newUser:function(req, res){
+        let errors = validationResult(req);
+        if (errors.isEmpty()) {
+            db.User.create({
             email: req.body.email,
             password: req.body.password,
-            
-        })
+            })
+    } else {
+        return res.render('register', {errors: errors.errors})
+    }
     }
 }
 
--- app-side/routes/register.js
+++ app-side/routes/register.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 let registerController = require('../controllers/registerController')
+let {check, validationResult, body} = require('express-validator');
 
 
 router.get('/',registerController.register);
-router.post('/',registerController.newUser);
+router.post('/', [
+    check('email').isEmail().withMessage('Invalid Email'),
+    check('password').isLength({min:6}).withMessage('Password must be 6-32 characters in length '),
+    ], 
+    registerController.newUser);
 module.exports = router;
